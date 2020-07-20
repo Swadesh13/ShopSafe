510e42b5cd5ed5d9de3d10b3410e0ed0cb4bddee
Swadesh Jana <swadeshjana@gmail.com>
2020-07-20 22:25:24 +0530

    Validation Updated

--- shopsafe-backend/functions/util/config.js
+++ shopsafe-backend/functions/util/config.js
@@ -18,4 +18,5 @@ module.exports = {
   messagingSenderId: "831437368791",
   appId: "1:831437368791:web:e4d584dd6f467a63572f92",
   measurementId: "G-12KL56Q4CT",
+  googleMapsApiKey: "AIzaSyBixtvcF5A38Z2dVP9fFkcvLf5P59RmnEA"
 };
